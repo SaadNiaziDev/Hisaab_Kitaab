46d47cdbb512aa8cf8574b68df0c4a5e89144df5
SaadNiaziDev <saad.niaxi47@gmail.com>
2022-01-10 20:38:10 +0500

    Design changes 2

--- app/src/main/java/com/example/hisaab_kitaab/MainActivity.java
+++ app/src/main/java/com/example/hisaab_kitaab/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.hisaab_kitaab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -13,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login = findViewById(R.id.signInBTN);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), HomeScreen.class);
+                startActivity(intent);
+            }
+        });
     }
 }
