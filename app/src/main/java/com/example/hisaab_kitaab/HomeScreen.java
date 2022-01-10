c9e1a414aeb09921c18f6c69007e3e2630174de4
SaadNiaziDev <saad.niaxi47@gmail.com>
2022-01-11 01:05:28 +0500

    RecyclerView Partial

--- app/src/main/java/com/example/hisaab_kitaab/HomeScreen.java
+++ app/src/main/java/com/example/hisaab_kitaab/HomeScreen.java
@@ -1,14 +1,19 @@
 package com.example.hisaab_kitaab;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class HomeScreen extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
     }
 }
