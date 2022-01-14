e6458e0c89042c6597640c443f4dad8ef69061bf
SaadNiaziDev <saad.niaxi47@gmail.com>
2022-01-15 02:14:57 +0500

    login/signup design complete

--- app/src/main/java/com/example/hisaab_kitaab/MainActivity.java
+++ app/src/main/java/com/example/hisaab_kitaab/MainActivity.java
@@ -9,19 +9,26 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button signInB,signUpBtn;
+    Button signInBtn,signUpBtn;
     TextView email,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportFragmentManager().beginTransaction().replace(R.id.fragmentLayout,new SignInFragment()).commit();
-        View login = findViewById(R.id.signInBtn);
-//        login.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentLayout,new SignInFragment()).commit();
-//            }
-//        });
+        signInBtn = findViewById(R.id.signInBtn);
+        signUpBtn = findViewById(R.id.signUpBtn);
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentLayout,new SignInFragment()).commit();
+            }
+        });
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentLayout,new SignUpFragment()).commit();
+            }
+        });
     }
 }
