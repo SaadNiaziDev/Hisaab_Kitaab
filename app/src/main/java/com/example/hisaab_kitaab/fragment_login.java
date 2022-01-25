c057cc3631ff562c48589dc80f92008481dc0d5b
Samim <bsamimzubair@gmail.com>
2022-01-25 22:56:57 +0500

    Icon added

--- app/src/main/java/com/example/hisaab_kitaab/fragment_login.java
+++ app/src/main/java/com/example/hisaab_kitaab/fragment_login.java
@@ -17,34 +17,38 @@ public class fragment_login extends Fragment {
     FirebaseAuth auth;
     Button loginBtn;
     String name;
-    EditText email,password;
+    EditText email, password;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        auth=FirebaseAuth.getInstance();
+        auth = FirebaseAuth.getInstance();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_login, container, false); // fragment inflates
-        email=rootView.findViewById(R.id.et_email);
-        password=rootView.findViewById(R.id.et_password);
-        loginBtn= rootView.findViewById(R.id.btn_login);
-        loginBtn.setOnClickListener(view -> auth.signInWithEmailAndPassword( // email and psswd verification
-                email.getText().toString(),
-                password.getText().toString())
-                .addOnCompleteListener(task -> {
-                    if(task.isSuccessful()){
-                        Intent intent = new Intent(getContext(),HomeScreen.class);
-                        startActivity(intent);
-                    }
-                    else
-                    {
-                        Toast.makeText(getActivity(), "Invalid Credentials!",Toast.LENGTH_SHORT).show();
-                    }
-                }));
+        email = rootView.findViewById(R.id.et_email);
+        password = rootView.findViewById(R.id.et_password);
+        loginBtn = rootView.findViewById(R.id.btn_login);
+        loginBtn.setOnClickListener(view -> {
+            if (email.getText().toString().isEmpty() || password.getText().toString().isEmpty()) {
+                Toast.makeText(getActivity(), "Invalid Credentials!", Toast.LENGTH_SHORT).show();
+            } else {
+                auth.signInWithEmailAndPassword( // email and psswd verification
+                        email.getText().toString(),
+                        password.getText().toString())
+                        .addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                Intent intent = new Intent(getContext(), HomeScreen.class);
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(getActivity(), "Invalid Credentials!", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
         return rootView;
     }
 
